c7bf2b8769600657e6dfe3a05f1badad6ff8ffed
xyz600 <xyz600600@gmail.com>
2019-03-22 16:26:10 +0900

    add test

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -4,7 +4,128 @@ using sudoku_sat_solver;
 using Xunit;
 
 namespace test {
-    public class UnitTest1 {
+
+    public class UnitTestExpression {
+
+        public ExpressionAnd GenerateTestAnd () {
+            var expr = new ExpressionAnd ();
+
+            var expr1 = new ExpressionAnd ();
+            expr1.children.Add (new ExpressionInteger (0, true));
+            expr1.children.Add (new ExpressionInteger (1, false));
+            expr.children.Add (expr1);
+
+            var expr2 = new ExpressionAnd ();
+            expr2.children.Add (new ExpressionInteger (2, true));
+            expr2.children.Add (new ExpressionInteger (3, false));
+            expr.children.Add (expr2);
+
+            return expr;
+        }
+
+        public ExpressionOr GenerateTestOr () {
+            var expr = new ExpressionOr ();
+
+            var expr1 = new ExpressionOr ();
+            expr1.children.Add (new ExpressionInteger (0, true));
+            expr1.children.Add (new ExpressionInteger (1, false));
+            expr.children.Add (expr1);
+
+            var expr2 = new ExpressionOr ();
+            expr2.children.Add (new ExpressionInteger (2, true));
+            expr2.children.Add (new ExpressionInteger (3, false));
+            expr.children.Add (expr2);
+            return expr;
+        }
+
+        public ExpressionAnd GenerateTestAndOr () {
+            var expr = new ExpressionAnd ();
+
+            var expr1 = new ExpressionOr ();
+            expr1.children.Add (new ExpressionInteger (0, true));
+            expr1.children.Add (new ExpressionInteger (1, false));
+            expr.children.Add (expr1);
+
+            var expr2 = new ExpressionOr ();
+            expr2.children.Add (new ExpressionInteger (2, true));
+            expr2.children.Add (new ExpressionInteger (3, false));
+            expr.children.Add (expr2);
+            return expr;
+        }
+
+        void CheckType<T> (IExpression exp) {
+            Assert.Equal (typeof (T), exp.GetType ());
+        }
+
+        [Fact]
+        public void TestExpressionInteger () {
+            var expr = new ExpressionInteger (0, true);
+            Assert.True (expr.id == 0);
+            Assert.True (expr.negative);
+        }
+
+        [Fact]
+        public void TextExpressionAndFlatten () {
+            var expr = GenerateTestAnd ();
+            var flatten = expr.Flatten ();
+            Assert.Equal (4, flatten.children.Count);
+            for (int i = 0; i < flatten.children.Count; i++) {
+                CheckType<ExpressionInteger> (flatten.children[i]);
+                ExpressionInteger subexpr = (ExpressionInteger) flatten.children[i];
+                Assert.Equal (i, subexpr.id);
+            }
+        }
+
+        [Fact]
+        public void TextExpressionOrFlatten () {
+            var expr = GenerateTestOr ();
+            var flatten = expr.Flatten ();
+            Assert.Equal (4, flatten.children.Count);
+            for (int i = 0; i < flatten.children.Count; i++) {
+                CheckType<ExpressionInteger> (flatten.children[i]);
+                ExpressionInteger subexpr = (ExpressionInteger) flatten.children[i];
+                Assert.Equal (i, subexpr.id);
+            }
+        }
+
+        [Fact]
+        public void TextExpressionAndOrFlatten () {
+            var expr = GenerateTestAndOr ();
+            var flatten = expr.Flatten ();
+            Assert.Equal (2, flatten.children.Count);
+            CheckType<ExpressionAnd> (expr);
+        }
+
+        [Fact]
+        public void TestNegate () {
+            var expr = GenerateTestAndOr ();
+            var negExpr = expr.Negate ();
+            int index = 0;
+            bool[] expected = { false, true, false, true };
+
+            CheckType<ExpressionOr> (negExpr);
+            foreach (var child in negExpr.children) {
+                CheckType<ExpressionAnd> (child);
+                foreach (var child2 in child.children) {
+                    CheckType<ExpressionInteger> (child2);
+                    var subexpr = (ExpressionInteger) child2;
+                    Assert.Equal (expected[index], subexpr.negative);
+                    Assert.Equal (index, subexpr.id);
+                    index++;
+                }
+            }
+        }
+
+        [Fact]
+        public void TestEmpty () {
+            var expOr = new ExpressionOr ();
+            var emptyOr = expOr.Empty ();
+            Assert.Equal (typeof (ExpressionOr), emptyOr.GetType ());
+
+        }
+    }
+
+    public class UnitTestLoader {
         [Fact]
         public void TestLoader () {
             var loader = new SudokuLoader ();
